075ed38d735d1f941a73a72fd86433a1786761dd
ArluAe <charles.andrea.gravier@gmail.com>
2024-02-04 22:35:39 +0100

    Initial Commit

---
+++ Spacematic/SpacematicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacematicApp.swift
+//  Spacematic
+//
+//  Created by Charles-An on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpacematicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
